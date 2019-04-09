a01b16d3f6d5527f3ed24e16114d41b0b7914d95
Shannen Lam <shannenlm@gmail.com>
2019-04-09 14:35:57 -0400

    changed listening port

--- index.js
+++ index.js
@@ -156,6 +156,10 @@ app.get("/bygenre", function(req, res) {
   });
 });
 
-app.listen(3000, function () {
+/*app.listen(3000, function () {
   console.log('Listening on port 3000!');
+}); */
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
